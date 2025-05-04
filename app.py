478f179ac354d9f4bd8446f2b50490797f5898db
leito121332 <leonardo48397@gmail.com>
2025-05-04 10:14:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -83,10 +83,3 @@ if __name__ == '__main__':
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return '¡Hola Mundo!'
-
-if __name__ == "__main__":
-    app.run(debug=True)
